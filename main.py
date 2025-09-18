bd26c6d1a55db8a80fb513b67b810c0fbe4bbb8f
Rajat Pednekar <rajatpednekar@Rajats-MacBook-Air.local>
2025-09-18 19:30:19 -0400

    Initail Commit: Calculator Project

---
+++ main.py
@@ -0,0 +1,8 @@
+
+from app.calculator import calculator
+
+
+if __name__ == "__main__":
+    # Now, we use the calculator tool we got earlier. This will start the calculator, which is a program 
+    # that keeps running and doing math based on what we tell it.
+    calculator()
